45c9cc0ebbc5d155c0709a0375be5688e21928c7
Greg McGuirk <bert.mcguirk@gmail.com>
2018-01-14 15:53:26 -0800

    updated email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,6 +3,6 @@ class InvitationMailer < ApplicationMailer
   def new_invitation(guest)
     @guest = guest
     @url = LinkBuilder.new(@guest).magic_link
-    mail(to: @guest.email, subject: "Invitation for #{@guest.name}")
+    mail(to: @guest.email, subject: "#{@guest.name} You're invited to Get Down!")
   end
 end
